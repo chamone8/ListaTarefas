0169f93222eca39f3b40b5b12d0d4f0dc5637e4d
felipe chamone <acessotichamone@hotmail.com>
2019-10-23 15:59:17 -0300

    Add project files.

---
+++ ListaTarefas/Migrations/20191023170639_Tafera_teste1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ListaTarefas.Migrations
+{
+    public partial class Tafera_teste1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tarefas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true),
+                    Inicio = table.Column<DateTime>(nullable: false),
+                    Fim = table.Column<DateTime>(nullable: false),
+                    Importancia = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarefas");
+        }
+    }
+}
---
+++ ListaTarefas/Models/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListaTarefas.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Tarefas> Tarefas  { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> op) : base(op)
+        { }
+    }
+}
